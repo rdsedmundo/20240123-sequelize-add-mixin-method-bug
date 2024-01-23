af84fd66e5d1f54f5f131aedc3e67f163652b1fb
Edmundo Santos <rdsedmundo@gmail.com>
2024-01-23 15:52:06 +0000

    reproduce

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -1,4 +1,4 @@
-import { DataTypes, Model } from 'sequelize';
+import { BelongsToManyAddAssociationMixin, DataTypes, Model } from 'sequelize';
 import { createSequelize6Instance } from '../setup/create-sequelize-instance';
 import { expect } from 'chai';
 import sinon from 'sinon';
@@ -21,13 +21,59 @@ export async function run() {
     },
   });
 
-  class Foo extends Model {}
+  class Foo extends Model {
+    public declare addBar: BelongsToManyAddAssociationMixin<Bar, string>;
+  }
+  class Bar extends Model {}
+  class FooBarAttachment extends Model {}
 
-  Foo.init({
-    name: DataTypes.TEXT,
-  }, {
-    sequelize,
-    modelName: 'Foo',
+  Foo.init(
+    {
+      fooName: {
+        type: DataTypes.TEXT,
+        primaryKey: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Foo',
+    }
+  );
+
+  Bar.init(
+    {
+      barName: {
+        type: DataTypes.TEXT,
+        primaryKey: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Bar',
+    }
+  );
+
+  FooBarAttachment.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fooName: DataTypes.TEXT,
+      barName: DataTypes.TEXT,
+      restrictionId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'FooBarAttachment',
+    }
+  );
+
+  Foo.belongsToMany(Bar, {
+    foreignKey: 'fooName',
+    otherKey: 'barName',
+    through: FooBarAttachment,
   });
 
   // You can use sinon and chai assertions directly in your SSCCE.
@@ -36,6 +82,88 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  // tests
+  const [foo, bar, anotherBar] = await Promise.all([
+    Foo.create({ fooName: 'TS foo' }),
+    Bar.create({ barName: 'TS bar' }),
+    Bar.create({ barName: 'TS anotherBar' }),
+  ]);
+
+  // First test: add bar to foo
+  await foo.addBar(bar, {
+    through: {
+      restrictionId: 1,
+    },
+  });
+  expect(
+    (await FooBarAttachment.findAll({ order: [['id', 'ASC']] })).map((x) =>
+      x.toJSON()
+    ),
+    'First test failed'
+  ).to.deep.include.members([
+    {
+      id: 1,
+      fooName: 'TS foo',
+      barName: 'TS bar',
+      restrictionId: 1,
+    },
+  ]);
+
+  // Second test: add anotherBar to foo
+  await foo.addBar(anotherBar, {
+    through: {
+      restrictionId: 2,
+    },
+  });
+  expect(
+    (await FooBarAttachment.findAll({ order: [['id', 'asc']] })).map((x) =>
+      x.toJSON()
+    ),
+    'Second test failed'
+  ).to.deep.include.members([
+    {
+      id: 1,
+      fooName: 'TS foo',
+      barName: 'TS bar',
+      restrictionId: 1,
+    },
+    {
+      id: 2,
+      fooName: 'TS foo',
+      barName: 'TS anotherBar',
+      restrictionId: 2,
+    },
+  ]);
+
+  // Third test: add bar to foo again, but with another restrictionId
+  await foo.addBar(bar, {
+    through: {
+      restrictionId: 3,
+    },
+  });
+  expect(
+    (await FooBarAttachment.findAll({ order: [['id', 'asc']] })).map((x) =>
+      x.toJSON()
+    ),
+    'Third test failed'
+  ).to.deep.include.members([
+    {
+      id: 1,
+      fooName: 'TS foo',
+      barName: 'TS bar',
+      restrictionId: 1,
+    },
+    {
+      id: 2,
+      fooName: 'TS foo',
+      barName: 'TS anotherBar',
+      restrictionId: 2,
+    },
+    {
+      id: 3,
+      fooName: 'TS foo',
+      barName: 'TS bar',
+      restrictionId: 3,
+    },
+  ]);
 }
